5b0d4eec79777cbced9eb10d3b7a0d1f4176ff03
Eranus <ecthomps@umich.edu>
2017-05-15 20:44:11 -0400

    added keyWordsNExpressions

---
+++ keyWordsNExpressions/src/com/chrisThomps/Main.java
@@ -0,0 +1,18 @@
+package com.chrisThomps;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //java has 53 reserved words
+        //all reserve words are represented in blue colors
+
+        //expression components = values, operands, variables, methods, controlFlowStatement
+        double kilo2Mile = (100 * 1.609344);
+        int highScore = 100;
+
+        if(highScore == 100)
+            System.out.println("This is an expression");
+    }
+}
