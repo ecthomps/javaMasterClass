56c996a1e07e0750715e7529378ff3dd48fa6d69
Eranus <ecthomps@umich.edu>
2017-05-16 23:52:17 -0400

    added javaClass

---
+++ javaClass/src/com/chrisThomps/Car.java
@@ -0,0 +1,14 @@
+package com.chrisThomps;
+
+/**
+ * Created by Christian Thompson on 5/16/2017.
+ */
+public class Car {
+    //a class is a blueprint for an object
+    //encapsulation in Java is used to hide the methods from being accessed publicly
+
+    //state components
+    private int door, wheels;
+    private String model, engine, color;
+
+}
---
+++ javaClass/src/com/chrisThomps/Main.java
@@ -0,0 +1,17 @@
+package com.chrisThomps;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //real-world object have two main characteristics
+        //state and behavior
+        //state is how much ram a computer has, the OS it's running
+        //hard drive size
+        //behavior for a computer is shutting down, restarting, outputting sound
+
+        Car porsche = new Car();
+        Car holden = new Car();
+    }
+}
