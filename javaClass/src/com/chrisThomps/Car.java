2dee4d6c3b35fe16a5393eefffff4601b06b3971
Eranus <ecthomps@umich.edu>
2017-05-25 15:38:27 -0400

    added class Definitions

--- javaClass/src/com/chrisThomps/Car.java
+++ javaClass/src/com/chrisThomps/Car.java
@@ -5,6 +5,14 @@ package com.chrisThomps;
  */
 public class Car {
     //a class is a blueprint for an object
+    //there are four access levels (Access modifiers)
+        /**
+         * public - everyone can access these instance variables
+         * private - only this class can access these variables
+         * protected - within the same package and/or derived class
+         *default - this is the default - if any other modifiers are missing
+         *          Java uses this one which means accessible within a package
+         * */
     //encapsulation in Java is used to hide the methods from being accessed publicly
 
     //behavior components
