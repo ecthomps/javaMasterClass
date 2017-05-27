6d9a7748eefe8225c6260865d3b4256310553cfe
Eranus <ecthomps@umich.edu>
2017-05-26 20:12:06 -0400

    added hashMap

--- hashMaps/src/com/chrisThomps/Main.java
+++ hashMaps/src/com/chrisThomps/Main.java
@@ -1,9 +1,29 @@
 package com.chrisThomps;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //a map is an associative data structure
+            //key1->value, key2->value, key3->value...
+        // a hashMap is a dictionary which holds key value pair
 
+        HashMap myHashMap = new HashMap();
+        myHashMap.put(1, "Bunny");
+        myHashMap.put("Lady", "Beautiful");
+        //myHashMap.value();  returns the value in the key
+        //returns true or false
+
+        if (myHashMap.containsKey(1))
+            System.out.println("Yes " + myHashMap.values());
+        else
+            System.out.println("Nope");
+
+        //myHashMap.remove("Lady");
+        //System.out.println("Value: " + myHashMap.values());
+
+       // System.out.println("Value: " + myHashMap.get(1));
     }
 }
