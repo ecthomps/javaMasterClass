ec16e2bc36ce9d5194ac50989b7734268f894634
Eranus <ecthomps@umich.edu>
2017-07-08 23:06:50 -0400

    added innerClasses

---
+++ javaOOP/InnerClasses/src/com/chrisThomps/GearBox.java
@@ -0,0 +1,20 @@
+package com.chrisThomps;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Christian Thompson on 7/8/2017.
+ */
+public class GearBox {
+
+    //inner class is a class withon another class
+    public GearBox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<Gear>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+    }
+
+    private ArrayList<Gear> gears;
+    private int maxGears, gearNumber = 0;
+}
---
+++ javaOOP/InnerClasses/src/com/chrisThomps/Main.java
@@ -0,0 +1,13 @@
+package com.chrisThomps;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //There are four different types of inner classes in Java
+        //Static Nested Classes, Member Inner Class, Method-Local Inner Class
+        //Anonymous Inner Class
+
+    }
+}
